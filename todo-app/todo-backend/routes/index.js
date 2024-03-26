57774d11b123d52486fd3b39839f70d4f2fafa1b
ddomhall <domjshall@gmail.com>
2024-03-26 04:34:15 +0000

    12.9

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const configs = require('../util/config')
+const redis = require('../redis')
 
 let visits = 0
 
@@ -16,3 +17,4 @@ router.get('/', async (req, res) => {
 });
 
 module.exports = router;
+
